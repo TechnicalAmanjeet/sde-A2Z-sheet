1860be0fec428ad6fdac2fbabbf7a5f52e01494d
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2023-08-18 19:17:00 +0530

    done

---
+++ src/Step001/Step001_01/NthFibonacciNumber.java
@@ -0,0 +1,26 @@
+package Step001.Step001_01;
+
+import java.util.Scanner;
+
+public class NthFibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n == 1 || n == 2) {
+            System.out.println(1);
+            return;
+        }
+
+        int prev = 1, curr = 1, temp = 0;
+
+        while (--n >= 2) {
+            temp = prev + curr;
+            prev = curr;
+            curr = temp;
+        }
+
+        System.out.println(curr);
+    }
+}
