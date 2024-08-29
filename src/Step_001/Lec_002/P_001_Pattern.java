636c84ea4abd1529780c4011a8980b410deff6a1
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2024-08-29 13:42:24 +0530

    done with 1st pattern solution

--- src/Step_001/Lec_002/P_001_Pattern.java
+++ src/Step_001/Lec_002/P_001_Pattern.java
@@ -2,6 +2,12 @@ package Step_001.Lec_002;
 
 public class P_001_Pattern {
     public static void main(String[] args) {
+//        N = 3
+
+//        * * *
+//        * * *
+//        * * *
+//
         Solution_P_001 sol = new Solution_P_001();
         sol.printFirstPattern(10);
     }
